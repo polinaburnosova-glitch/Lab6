21677ee4079561157f3929fa6b761879d41c2d10
Полина <polinaburnosova@gmail.com>
2026-04-21 12:38:19 +0300

    изменен build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    id("java")
-    id("application")
+    java
 }
 
-group = "org.example"
+group = "com.lab6"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -11,48 +10,37 @@ repositories {
 }
 
 dependencies {
-    // Jackson для работы с XML
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-
-    // Apache Commons Lang3 для ToStringBuilder
-    implementation("org.apache.commons:commons-lang3:3.13.0")
-}
-
-application {
-    mainClass.set("server.ServerMain")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.2")
+    implementation("com.fasterxml.woodstox:woodstox-core:6.5.1")
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
 
-tasks.withType<JavaExec> {
-    systemProperty("file.encoding", "UTF-8")
-}
-
-
-tasks.withType<JavaExec> {
-    systemProperty("file.encoding", "UTF-8")
-    systemProperty("console.encoding", "UTF-8")
-    jvmArgs = listOf("-Dfile.encoding=UTF-8")
-}
-
-dependencies {
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.15.2")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
-    implementation("org.apache.commons:commons-lang3:3.13.0")
-}
-
-tasks.register("runClient", JavaExec::class) {
-    mainClass.set("client.ClientMain")
-    classpath = sourceSets["main"].runtimeClasspath
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+    options {
+        this as StandardJavadocDocletOptions
+        charSet = "UTF-8"
+        docEncoding = "UTF-8"
+        addStringOption("Xdoclint:none", "-quiet")
+    }
+    isFailOnError = false
 }
 
 tasks.jar {
     manifest {
         attributes["Main-Class"] = "server.ServerMain"
     }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
